0a57625a1c52e396beb7fe41458e92014fdf478e
jcampa <jdelacampa101@gmail.com>
2016-03-06 13:35:17 -0400

    Update 4.Vector.for.c

--- 4.Vector.for.c
+++ 4.Vector.for.c
@@ -16,17 +16,17 @@ for (i = 0; i < l; i++) {
 L2:
         LDAA    i         // cargar la variable i al registro A
         CMPA    l         // i - l
-        BHS     L1      // i - l >= 0 ?
+        BHS     L1        // i - l >= 0 ?
 
-        LDX     v         // cargar el vector v
-        LDAB    i         // cargarmos la variable i al registro B
-        ABX               // apuntamos a la direccion v + i
-        LDDA    s         // cargamos s
-        ADDA    O, x      // 0 saltos despues x
-        STAA    s         // guardamos el registro A en la variale s
+        LDX     v         // X = v        cargar el vector v
+        LDAB    i         // B = i        cargarmos la variable i al registro B
+        ABX               // X = v + i    apuntamos a la direccion v + i
+        LDDA    s         // A = s        cargamos s
+        ADDA    O, x      // A += v[i]    0 saltos despues x
+        STAA    s         // s = A        guardamos el registro A en la variale s
 
         inc     i         // i++
-        BRA     L2      // volvemos al for
+        BRA     L2        // volvemos al for
 L1:
 /* <code here> */
 //  K&R
