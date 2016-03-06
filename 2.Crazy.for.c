4bd22ef098f825cabfcb0b96a797fbfc4e93adf6
jcampa <jdelacampa101@gmail.com>
2016-03-06 02:23:03 -0400

    Update 2.Crazy.for.c

--- 2.Crazy.for.c
+++ 2.Crazy.for.c
@@ -16,25 +16,27 @@ for (i = l + 1; i < l + 1; i*=2) {
         STAA    i
 
 // i - l - 1 >= 0
-L2
+L2:
         LDAA    i
         DECA
         CMPA    l //A –M
         BGE     L1 // branch if >= zero
-/* if  l - i >= 0*/
 
-              LDAA    l
-              CMPA    i
-              BLE     <L3>
+/* if  l - i >= 0*/
+        LDAA    l
+        CMPA    i
+        BLE     <L3>
+        
 /* l = i - l*/
-              LDAA    i
-              SUBA    l
-              STAA    l
+        LDAA    i
+        SUBA    l
+        STAA    l
 
 L3:
 /* i*=2 */
-              LDAA    i
-              ADDA    i
-              STAA    i
-              JMP     <L2>
+        LDAA    i
+        ADDA    i
+        STAA    i
+        JMP     <L2>
 L1:
+//
